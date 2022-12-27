0e5c295a5359635a8d0f27d6abeedd5898f1b6bd
wwhfree <wwhfree@qq.com>
2022-12-27 16:00:15 +0800

    Site updated: 2022-12-27 16:00:15

--- js/he-simple-common.js
+++ js/he-simple-common.js
@@ -3,7 +3,8 @@
   c.rel = 'stylesheet'
   c.href = '../css/he-simple.css'
   var s = d.createElement('script')
-  s.src = 'https://widget.heweather.net/simple/static/js/he-simple.js?v=1.4.0'
+  // s.src = 'https://widget.heweather.net/simple/static/js/he-simple.js?v=1.4.0'
+  s.src = './he-simple.js'
   var sn = d.getElementsByTagName('script')[0]
   sn.parentNode.insertBefore(c, sn)
   sn.parentNode.insertBefore(s, sn)
